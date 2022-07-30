afa9065e6bd19319bc1bd3a734f816f61688e26b
Dan Ortega <ortega.dan2010@gmail.com>
2022-07-30 10:44:28 -0500

    comment

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -7,6 +7,7 @@ mycalfile=your.email@address.com.ics
 if [ -z "$1" ]; then
     echo "Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument"
 else
+    # adjust with your prefered default usage
     tsvf=$(icsreader $mycalfile $1 noclient true | grep exported | cut -f 4 -d ' ')
     converthourstobars $tsvf
     xdg-open $tsvf
