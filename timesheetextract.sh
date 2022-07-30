71c503a9704176400e289f67ba477342ab74747d
Dan Ortega <ortega.dan2010@gmail.com>
2022-07-30 10:32:37 -0500

    improved helper comments

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
+# custom runner helper
 
-mycal=your.email@address.com
+# replace with your common calendar ICS file
+mycalfile=your.email@address.com.ics
 
 if [ -z "$1" ]; then
     echo "Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument"
 else
-    tsvf=$(icsreader $mycal $1 noclient true | grep exported | cut -f 4 -d ' ')
+    tsvf=$(icsreader $mycalfile $1 noclient true | grep exported | cut -f 4 -d ' ')
     converthourstobars $tsvf
     open $tsvf
 fi
