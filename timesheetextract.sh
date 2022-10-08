57fc75a1cb39fced6494985f350a9bb9f84f1634
Dan Ortega <ortega.dan2010@gmail.com>
2022-10-07 20:41:46 -0500

    adjusting info

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -3,6 +3,8 @@
 
 # replace with your common calendar ICS file
 mycalfile=your.email@address.com.ics
+# or use an env variable
+# mycalfile=$mycal
 
 if [ -z "$1" ]; then
     echo "Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument"
@@ -10,6 +12,9 @@ else
     # adjust with your prefered default usage
     tsvf=$(icsreader $mycalfile $1 noclient true | grep exported | cut -f 4 -d ' ')
     converthourstobars $tsvf
+    # open with default application
+    # on linux
     xdg-open $tsvf
+    # on mac
     # open $tsvf
 fi
