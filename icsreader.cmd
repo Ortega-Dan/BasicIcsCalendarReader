6abc3ac810c60cddfa1ef9e54c7e613e95d4edd7
Dan Ortega <ortega.dan2010@gmail.com>
2023-02-02 20:32:16 -0500

    adding scripts for jar support in windows

---
+++ icsreader.cmd
@@ -0,0 +1,1 @@
+java -jar C:\Users\daos_\.maOwnBin\icsreader.jar %*
---
+++ timesheetextract.cmd
@@ -0,0 +1,16 @@
+@echo off
+REM custom runner helper
+
+REM replace with your common calendar ICS file
+set mycalfile=ortega.dan2010@gmail.com.ics
+REM or use an env variable
+REM mycalfile=$mycal
+
+IF [%1]==[] (
+echo Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument
+) ELSE (
+icsreader %mycalfile% %1 noclient true
+
+REM NOTE: this doesn't support triggering converthourstobars yet
+
+)
