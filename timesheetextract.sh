24e649f08a791d7d2aaf8c4f4387488d42408275
Dan Ortega <ortega.dan2010@gmail.com>
2023-03-30 18:48:38 -0500

    comment adjust

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -4,7 +4,7 @@
 # replace with your common calendar ICS file
 MYCALFILE=your.email@address.com.ics
 # or remove previous line and use an env variable
-# mycalfile=$mycal
+# MYCALFILE=$mycal
 
 if [ -z "$1" ]; then
     echo "Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument"
