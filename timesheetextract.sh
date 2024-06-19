01239627d6c2d6b9044ca0bc89fb40e282a0ec58
Dan Ortega <ortega.dan2010@gmail.com>
2024-06-19 16:30:32 -0500

    improved with auto unzip

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -2,20 +2,23 @@
 # custom runner helper
 
 # replace with your common calendar ICS file
-MYCALFILE=your.email@address.com.ics
+CAL_EMAIL=your.email@address.com.ics
 # or remove previous line and use an env variable
-# MYCALFILE=$mycal
+# CAL_EMAIL=$MY_CAL_EMAIL_ENV_VAR
+CAL_FILE="$CAL_EMAIL.ics"
+CAL_ZIPFILE="$CAL_EMAIL.ical.zip"
 
 if [ -z "$1" ]; then
     echo "Provide the date or dates range as YYYY-MM-DD or YYYY-MM-DD_YYYY-MM-DD in a single argument"
 else
+    unzip -u $CAL_ZIPFILE
     # adjust with your prefered default usage
-    tsvf=$(icsreader $MYCALFILE $1 noclient false | grep "File exported to" | cut -f 4 -d ' ')
+    tsvf=$(icsreader $CAL_FILE $1 noclient false | grep "File exported to" | cut -f 4 -d ' ')
     withbars="$tsvf.withbars.tsv"
     converthourstobars $tsvf > $withbars
     # open with default application
     # on linux
-    xdg-open $withbars
+    # xdg-open $withbars
     # on mac
-    # open $withbars
+    open $withbars
 fi
