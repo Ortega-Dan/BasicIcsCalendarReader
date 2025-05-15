a61e6f866d361f91be3ab5768ad4acf594209d14
Dan Ortega <ortega.dan2010@gmail.com>
2025-05-15 12:24:48 -0500

    timesheet extract refinements to support caledars with spaces in the name

--- timesheetextract.sh
+++ timesheetextract.sh
@@ -3,9 +3,9 @@
 
 # replace with your common calendar ICS file
 CAL_EMAIL=your.email@address.com.ics
-# or remove previous line and use an env variable
-# CAL_EMAIL=$MY_CAL_EMAIL_ENV_VAR
-CAL_FILE="$CAL_EMAIL.ics"
+CAL_PREFIX="" # a specific calender prefix, if needed (with the included trailing underscore _ to find it after unzipping). e.g. "Time Tracking_"
+
+CAL_FILE="$CAL_PREFIX$CAL_EMAIL.ics"
 CAL_ZIPFILE="$CAL_EMAIL.ical.zip"
 
 if [ -z "$1" ]; then
@@ -13,12 +13,16 @@ if [ -z "$1" ]; then
 else
     unzip -o $CAL_ZIPFILE
     # adjust with your prefered default usage
-    tsvf=$(icsreader $CAL_FILE $1 noclient false | grep "File exported to" | cut -f 4 -d ' ')
+    echo "Unzipped. Attempting to extract timesheet data from '$CAL_FILE'"
+    tsvf=$(icsreader "$CAL_FILE" "$1" | grep "File exported to" | cut -f 4- -d ' ')
+    echo "Extracted timesheet data to $tsvf"
     withbars="$tsvf.withbars.tsv"
-    converthourstobars $tsvf > $withbars
+    echo "Converting to bars format"
+    converthourstobars "$tsvf" > "$withbars"
     # open with default application
     # on linux
     # xdg-open $withbars
     # on mac
-    open $withbars
+    open "$withbars"
+    open "$tsvf"
 fi
